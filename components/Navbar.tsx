e706b7d745e55d6a9c81a1dcebc7ae97f75dc1d8
Rod Garland <rg6797@outlook.com>
2022-08-15 22:39:54 -0400

    Slight alterations to the navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,41 +1,35 @@
 import React, { useState } from 'react';
 import  ApexLogo  from '../assets/logo_the_web_apex.svg';
 import Link from 'next/link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBarsStaggered, faAngleDown, faToggleOff, faToggleOn } from '@fortawesome/free-solid-svg-icons';
 import { useGetCategoriesQuery } from '../graphql/generated/schema';
 
 
 
 const Navbar = () => {
-    const [categories, setCategories] = useState<any>([]);
 
-    const {data} = useGetCategoriesQuery({
-        onCompleted: (data) => {
-            setCategories(data.categories?.data)
-        }
-    })
 
     return (
-        <div className="flex py-4 mx-auto my-0 rounded-lg px-4 items-center ">
-
-        <div className=" flex-1">
-            <Link href='/'>
-                <ApexLogo className="h-16 w-32  bg-[#89d8d3] bg-gradient-to-br from-[#89d8d3]/0 to-[#03c8a8]/75 rounded-full py-2 cursor-pointer md:h-16"/>
-            </Link>
-        </div>       
-
-        <div className="flex-none">
-            <div className="menu menu-horizontal space-x-6">
-               {/* {categories.map((category:any) => (
-                    <Link key={category.attributes.Slug} href={`/category/${category.attributes.Slug}`}>
-                        <span className=' cursor-pointer font-bold text-lg text-white  hover:border-b-2 border-spacing-2 hover:border-[#03c8a8]'>
-                            {category.attributes.Name}
-                        </span>
-                    </Link>
-               ))} */}
+        <nav className="flex justify-between py-4 mx-auto my-0  px-4 items-center  ">
+            <div className=' flex items-center center cursor-pointer space-x-2'>
+                <FontAwesomeIcon icon={faBarsStaggered}/>
+                <p className=' font-semibold '>Categories</p>
+                <FontAwesomeIcon icon={faAngleDown}/>
             </div>
-        </div>
+            <div className="">
+                <Link href='/'>
+                    <ApexLogo className=" h-16 w-32   bg-[#89d8d3] bg-gradient-to-br from-[#89d8d3]/0 to-[#03c8a8]/75 rounded-full py-2 cursor-pointer"/>
+                </Link>
+            </div>       
 
-      </div>
+            <div className="">
+                <div className="menu menu-horizontal space-x-6">
+                    <FontAwesomeIcon icon={faToggleOff} className=' text-5xl'/>
+                    {/* <FontAwesomeIcon icon={faToggleOn} className=' text-5xl'/> */}
+                </div>
+            </div>
+      </nav>
     );
 };
 
