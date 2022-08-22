47aefda2e79f6ce3d66cf046b0add0fe51e71307
Rod Garland <rg6797@outlook.com>
2022-08-22 00:51:08 -0400

    Added dropdown categories menu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,10 +7,19 @@ import {
   faAngleDown,
 } from "@fortawesome/free-solid-svg-icons";
 import { useTheme } from "next-themes";
+import { useGetCategoriesQuery } from "../graphql/generated/schema";
 
 const Navbar = () => {
   const { theme, setTheme } = useTheme();
   const [checked, setChecked] = useState<boolean>(false);
+
+  const [categories, setCategories] = useState<any>([]);
+
+  const { data } = useGetCategoriesQuery({
+    onCompleted: (data) => {
+      setCategories(data.categories?.data);
+    },
+  });
 
   const themeChange = () => {
     if (checked) {
@@ -22,10 +31,30 @@ const Navbar = () => {
 
   return (
     <nav className="mx-auto my-0 flex items-center justify-between py-4  px-4">
-      <div className=" center flex cursor-pointer items-center space-x-2 text-light-primary dark:text-dark-primary">
-        <FontAwesomeIcon icon={faBarsStaggered} />
-        <p className=" font-semibold ">Categories</p>
-        <FontAwesomeIcon icon={faAngleDown} />
+      <div className="dropdown">
+        <label
+          tabIndex={0}
+          className="flex cursor-pointer items-center space-x-2 text-light-primary dark:text-dark-primary"
+        >
+          <FontAwesomeIcon icon={faBarsStaggered} />
+          <p className=" font-semibold ">Categories</p>
+          <FontAwesomeIcon icon={faAngleDown} />
+        </label>
+        <ul
+          tabIndex={0}
+          className="dropdown-content menu rounded-box mt-4 ml-4  w-52 bg-light-primary bg-gradient-to-br  from-primary/0 to-secondary/75 py-2 dark:bg-dark-primary dark:from-light-primary/0 dark:to-dark-secondary"
+        >
+          {categories.map((category: any) => (
+            <li
+              key={category.attributes.Slug}
+              className="mb-3 cursor-pointer pb-3 font-semibold text-light-neutral hover:text-light-accent  hover:underline dark:text-dark-neutral hover:dark:text-dark-accent"
+            >
+              <Link href={`/category/${category.attributes.Slug}`}>
+                {category.attributes.Name}
+              </Link>
+            </li>
+          ))}
+        </ul>
       </div>
       <div className="">
         <Link href="/">
