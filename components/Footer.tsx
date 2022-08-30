b7f068613928fedd21d5fba575aa40a8cb98367d
Rod Garland <rg6797@outlook.com>
2022-08-30 00:13:28 -0400

    Fixed Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -11,10 +11,10 @@ import { IconProp } from "@fortawesome/fontawesome-svg-core";
 
 const Footer = () => {
   return (
-    <footer className=" footer items-center bg-light-primary bg-gradient-to-br from-light-primary/0 to-light-secondary/75 p-2  dark:bg-dark-primary dark:from-dark-primary/0 dark:to-dark-secondary">
+    <footer className=" footer fixed bottom-0 h-16 w-full items-center bg-light-primary bg-gradient-to-br from-light-primary/0 to-light-secondary/75 p-2  dark:bg-dark-primary dark:from-dark-primary/0 dark:to-dark-secondary">
       <div className="grid-flow-col items-center">
         <ApexLogo className="h-12 w-36 cursor-pointer md:h-16" />
-        <p className=" text-white font-bold">
+        <p className=" font-bold text-white">
           © 2022 The Web Apex Blogs - @zencoder24{" "}
         </p>
       </div>
@@ -22,19 +22,19 @@ const Footer = () => {
         <Link href="https://github.com/zencoder24">
           <FontAwesomeIcon
             icon={faGithub as IconProp}
-            className="text-white h-6 w-6 cursor-pointer "
+            className="h-6 w-6 cursor-pointer text-white "
           />
         </Link>
         <Link href="https://www.linkedin.com/in/rodderick-garland-58ba19160/">
           <FontAwesomeIcon
             icon={faLinkedin as IconProp}
-            className="text-white h-6 w-6 cursor-pointer "
+            className="h-6 w-6 cursor-pointer text-white "
           />
         </Link>
         <Link href="www.twitter.com">
           <FontAwesomeIcon
             icon={faTwitter as IconProp}
-            className="text-white h-6 w-6 cursor-pointer "
+            className="h-6 w-6 cursor-pointer text-white "
           />
         </Link>
       </div>
